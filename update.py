52f3c5d486bb4c24a44227385b64f3c97cd4047c
hudbeard <hudbeard@gmail.com>
2025-03-21 15:23:45 -0500

    update local files

--- update.py
+++ update.py
@@ -5,7 +5,7 @@ import subprocess
 class Updator(object):
     def __init__(self):
         self.REPO_URL = "https://github.com/hudbeard/coaxial_drone_code.git"
-        self.LOCAL_DIR = os.path.join(os.path.dirname(os.path.abspath(__file__)), "repo")
+        self.LOCAL_DIR = os.path.dirname(os.path.abspath(__file__))
 
     def download_project(self):
         if not os.path.exists(os.path.join(self.LOCAL_DIR, ".git")):
