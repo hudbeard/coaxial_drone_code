36f45006bb8d5f9fdb1ddeb1f19509adc2d8ace5
hudbeard <hudbeard@gmail.com>
2025-03-21 15:31:18 -0500

    different lib for mpu

--- MPU_calibrate.py
+++ MPU_calibrate.py
@@ -1,5 +1,5 @@
 import smbus
-import mpu6050
+from MPU6050 import MPU6050
 
 
 class Drone(object):
@@ -8,11 +8,15 @@ class Drone(object):
         self.MPU_Y_ROTATION_OFFSET = 0
         self.MPU_Z_ROTATION_OFFSET = 0
 
-        self.mpu = mpu6050.mpu6050(0x68)
+        self.mpu = MPU6050.mpu6050(0x68)
+        self.mpu.reset()
+        self.mpu.power_manage()
+        self.mpu.gyro_config()
+        self.mpu.accel_config()
 
     def calibrate(self):
         while True:
-            print(self.mpu.get_gyro_data().values())
+            print(self.mpu.read_gyroscope())
 
 
 if __name__ == "__main__":
